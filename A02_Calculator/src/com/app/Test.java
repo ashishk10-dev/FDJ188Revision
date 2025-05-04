9a6b5039ba101eb00278dddeda1a42ae99b6a7c9
Ashish <ashishkamble1013@gmail.com>
2025-05-04 23:28:30 +0530

    Addition Method Written by Ashish

---
+++ A02_Calculator/src/com/app/Test.java
@@ -0,0 +1,18 @@
+package com.app;
+
+public class Test {
+
+	public void addition()
+	{
+		int x=18;
+		int y=20;
+		int z=x+y;
+		System.out.println("x + y : "+z);
+	}
+	
+	public static void main(String[] args) {
+		Test t=new Test();
+		t.addition();
+				
+	}
+}
