f6407fb18fb32f5c02d883c7ab7937fc7c59322a
Ashish <ashishkamble1013@gmail.com>
2025-05-04 23:34:41 +0530

    Substraction Method written by Ashish

--- A02_Calculator/src/com/app/Test.java
+++ A02_Calculator/src/com/app/Test.java
@@ -1,18 +1,29 @@
 package com.app;
 
 public class Test {
-
+	int x=18;
+	int y=20;
+	
 	public void addition()
 	{
-		int x=18;
-		int y=20;
+		
 		int z=x+y;
 		System.out.println("x + y : "+z);
+	}
+	
+//Substraction Method
+	public void substraction()
+	{
+		
+		int z=x-y;
+		System.out.println("x - y : "+z);
 	}
 	
 	public static void main(String[] args) {
 		Test t=new Test();
 		t.addition();
+		
+		t.substraction();
 				
 	}
 }
